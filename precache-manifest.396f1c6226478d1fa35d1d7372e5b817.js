6eef382ec2e9f503bafc4f585c7728c65c1f8c6f
Alexey Kulikov <alexeyqu@gmail.com>
2020-04-12 17:22:18 +0100

    Updates

---
+++ precache-manifest.396f1c6226478d1fa35d1d7372e5b817.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73ccb72b21160755d6d8f29ba2c18d3b",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "097d3f309ba5995ef0ef",
+    "url": "/react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c3f35d756ed98c3839a3",
+    "url": "/react-app/static/js/2.aeb10df2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.aeb10df2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "097d3f309ba5995ef0ef",
+    "url": "/react-app/static/js/main.83d04e0a.chunk.js"
+  },
+  {
+    "revision": "5d0376c2405e9e29a9b2",
+    "url": "/react-app/static/js/runtime-main.d5ecc926.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
